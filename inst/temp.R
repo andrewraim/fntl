4b75ead13fa293af84fc145ad357ea398d51796a
Andrew Raim <andrew.raim@gmail.com>
2026-01-17 19:34:07 -0500

    Part of mdarray apply implementation

--- inst/temp.R
+++ inst/temp.R
@@ -1,5 +1,5 @@
 library(Matrix)
-Rcpp::sourceCpp("temp.cpp", rebuild = TRUE)
+Rcpp::sourceCpp("inst/temp.cpp", rebuild = TRUE)
 
 xx = array(1:24, dim = c(2,3,4))
 temp_ex(xx)
--- inst/temp.cpp
+++ inst/temp.cpp
@@ -4,18 +4,41 @@
 // [[Rcpp::export]]
 Rcpp::IntegerVector temp_ex(const Rcpp::IntegerVector& xx)
 {
+	Rprintf("Begin\n");
+
 	fntl::mdarray<int, 3> x(xx);
 	// fntl::mdarray<int, 3> x({ 2, 2, 3 });
 
-	fntl::coord_t<3> dims = x.dim();
+	fntl::coord_t<3> dim = x.dim();
 
-	for (unsigned int k = 0; k < dims[2]; k++) {
-		for (unsigned int j = 0; j < dims[1]; j++) {
-			for (unsigned int i = 0; i < dims[0]; i++) {
+	for (unsigned int k = 0; k < dim[2]; k++) {
+		for (unsigned int j = 0; j < dim[1]; j++) {
+			for (unsigned int i = 0; i < dim[0]; i++) {
 				x({i, j, k}) *= 10;
 			}
 		}
 	}
 
-	return x.to_Vector<INTSXP>();
+	Rcpp::print(x.to_Vector<INTSXP>());
+
+
+	fntl::mdarray<int, 3> arr(xx);
+
+	std:: function f = [](const fntl::mdarray<int,1>& x) -> int
+	{
+		int out = 0;
+
+		fntl::coord_t<1> dim = x.dim();
+		Rprintf("dim[0] = %d\n", dim[0]);
+		for (unsigned int i = 0; i < dim[0]; i++) {
+			out += x({i});
+		}
+
+		Rprintf("sum = %d\n", out);
+		return out;
+	};
+	fntl::coord_t<2> margins = {2, 3};
+	const fntl::mdarray<int,2>& out = apply(arr, margins, f);
+
+	return out.to_Vector<INTSXP>();
 }
