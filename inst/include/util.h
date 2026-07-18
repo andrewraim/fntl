296fc868d813b051485532b00c27cf55497d2041
Andrew Raim <andrew.raim@gmail.com>
2026-07-18 17:27:34 -0400

    Move logger function here from vws package

--- inst/include/util.h
+++ inst/include/util.h
@@ -3,6 +3,28 @@
 
 namespace fntl {
 
+inline void logger(const char* fmt, ...)
+{
+	// Get the current time in the local time zone
+	std::time_t raw = std::time(nullptr);
+	std::tm* local = std::localtime(&raw);
+
+	// Write formatted time to a string
+	char buffer[64];
+	strftime(buffer, 64, "%Y-%m-%d %H:%M:%S", local);
+
+	// Insert "..." placeholders into format string for message; see
+	// <https://stackoverflow.com/q/1056411>
+	char msg[256];
+	va_list args;
+	va_start(args, fmt);
+	vsnprintf(msg, 255, fmt, args);
+	va_end(args);
+
+	// Print the formatted message with timestamp
+	Rprintf("%s - %s", buffer, msg);
+}
+
 inline std::string paste(const Rcpp::StringVector& x, const std::string& delim)
 {
 	std::string out;
